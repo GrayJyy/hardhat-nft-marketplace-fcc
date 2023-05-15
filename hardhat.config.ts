2e8ecee07a08902372772f6a2671b9c249d86019
GrayJiang <Gray_Jy@yeah.net>
2023-05-16 00:41:28 +0800

    feat: basic config

---
+++ hardhat.config.ts
@@ -0,0 +1,42 @@
+import { HardhatUserConfig } from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
+import { config as getConfig } from 'dotenv'
+import 'hardhat-deploy'
+getConfig()
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY!
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKET_API_KEY = process.env.COINMARKET_API_KEY!
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      { version: '0.8.18' },
+      { version: '0.8.7' },
+      { version: '0.8.0' },
+      {
+        version: '0.6.6',
+      },
+      { version: '0.6.0' },
+      {
+        version: '0.6.12',
+      },
+      {
+        version: '0.4.19',
+      },
+    ],
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: { chainId: 31337 },
+    localhost: { chainId: 31337 },
+    sepolia: { chainId: 11155111, url: SEPOLIA_RPC_URL, accounts: [PRIVATE_KEY], gas: 2100000, gasPrice: 8000000000 },
+  },
+  namedAccounts: { deployer: { default: 0, 1: 0 } },
+  etherscan: { apiKey: ETHERSCAN_API_KEY },
+  gasReporter: { enabled: false, coinmarketcap: COINMARKET_API_KEY },
+  mocha: { timeout: 500000 },
+}
+
+export default config
---
+++ helper-hardhat-config.ts
@@ -0,0 +1,11 @@
+export type ConfigType = { name: string }
+export type NetworkConfigType = { [key: number]: ConfigType }
+const networkConfig: NetworkConfigType = {
+  31337: { name: 'localhost' },
+  11155111: { name: 'sepolia' },
+}
+
+const developmentChains = ['localhost', 'hardhat']
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+
+export { networkConfig, developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS }
